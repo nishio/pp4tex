fdd0933d516a1f97fb95451093f1cb0b0856ec73
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2011-04-14 22:26:27 +0900

    fix bug: don't move when failed

--- go.sh
+++ go.sh
@@ -1,3 +1,2 @@
 base=`basename $1 .tex` 
-python pp4tex.py $1 && platex pp_$1 && dvipdfmx pp_$base.dvi
-mv pp_$base.pdf $base.pdf
+python pp4tex.py $1 && platex pp_$1 && dvipdfmx pp_$base.dvi && mv pp_$base.pdf $base.pdf
