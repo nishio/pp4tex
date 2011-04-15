5e1ef4154178829acb2ba86ba1509f9b380fa09c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2011-04-15 20:41:56 +0900

    refactoring

--- pp4tex.py
+++ pp4tex.py
@@ -35,14 +35,54 @@ class Namespace(dict):
         self[key] = value
 
 ns = Namespace()
-top = ns
-
 PUSH_PAT = re.compile("\s*%\s*push")
 POP_PAT = re.compile("\s*%\s*pop")
 DEFINE_PAT = re.compile("\s*%\s*define ([^[\s]+)(?:\[(\d+)\])?\s+(.+)")
 DEFINE_ML_PAT = re.compile("\s*%\s*define ([^[\s]+)(?:\[(\d+)\])?:")
 DEFINE_END_PAT = re.compile("\s*%\s*end")
 
+
+class SingleLineDef(object):
+    def __init__(self, body, args):
+        self.body = subst(body, ns)
+        if args:
+            self.args = int(args)
+        else:
+            self.args = 0
+
+    def eval(self, tree, i):
+        if not self.args:
+            return i, self.body
+        
+        tmp_ns = Namespace(parent=ns)
+        for j in range(self.args):
+            tmp_ns.set("#%d" % (j + 1), SingleLineDef(tree[i], None))
+            i += 1
+
+        return i, subst(self.body, tmp_ns)
+
+        
+
+class MultiLineDef(object):
+    def __init__(self, body, args):
+        self.body = [subst(line, ns) for line in body]
+        if args:
+            self.args = int(args)
+        else:
+            self.args = 0
+
+    def eval(self):
+        if not self.args:
+            return self.body
+        
+        tmp_ns = Namespace(parent=ns)
+        for j in range(self.args):
+            tmp_ns.set("#%d" % (j + 1), (tree[i], None))
+            i += 1
+
+        return i, subst(body, tmp_ns)
+
+        
 def traverse(tree, ns):
     new_tree = []
     i = 0
@@ -56,47 +96,68 @@ def traverse(tree, ns):
             if not new_item:
                 new_tree.append(item)
             else:
-                body, arg = new_item
-                if arg == None:
-                    new_tree.extend(body)
-                else:
-                    # with argument
-                    num = int(arg)
-                    ns = Namespace(parent=ns)
-                    for j in range(num):
-                        ns.set("#%d" % (j + 1), (tree[i], None))
-                        i += 1
-
-                    new_tree.extend(subst(body, ns))
-                    ns = ns.parent
+                i, tokens = new_item.eval(tree, i)
+                new_tree.extend(tokens)
 
     return new_tree
 
 
 def subst(s, ns):
-    tree = texparse.parse(s)
+    if isinstance(s, str):
+        tree = texparse.parse(s)
+    else:
+        tree = s
     new_tree = traverse(tree, ns)
     s = texparse.unparse(new_tree, True)
     return s
 
+
+def assert_is_single_token(frm, lno, **kw):
+    fs = texparse.parse(frm)
+    if len(fs) != 1:
+        raise RuntimeError(
+            "line %d: key '%s' should have only one token but has %s" %
+            (lno + 1, frm, fs))
+            
+
 def main():
-    for lno, line in enumerate(fi):
+    global ns
+    lno = 0
+    while True:
+        line = fi.readline()
+        if not line: break
+        lno += 1
+
         if PUSH_PAT.match(line):
             ns = Namespace(parent=ns)
             continue
+
         if POP_PAT.match(line):
             ns = ns.parent
             continue
+
         m = DEFINE_PAT.match(line)
         if m:
             frm, arg, to =  m.groups()
-            fs = texparse.parse(frm)
-            if len(fs) != 1:
-                raise RuntimeError(
-                    "line %d: key '%s' should have only one token but has %s" %
-                    (lno + 1, frm, fs))
+            assert_is_single_token(**locals())
+            ns.set(frm, SingleLineDef(to, arg))
+            continue
 
-            ns.set(frm, (subst(to, ns), arg))
+        m = DEFINE_ML_PAT.match(line)
+        if m:
+            frm, arg =  m.groups()
+            assert_is_single_token(**locals())
+            body = []
+            while True:
+                line = fi.readline()
+                if not line:
+                    raise RuntimeError("multiline definition '%s' is incomplete" % frm)
+                lno += 1
+                if DEFINE_END_PAT.match(line):
+                    break
+                body.append(line)
+            
+            ns.set(frm, MultiLineDef(body, args))
             continue
 
         line = subst(line, ns)
